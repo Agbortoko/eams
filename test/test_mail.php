da86cf8c19b51854c21eeba7864cee8c81b9cd84
Agbortoko <arreyagbortoko@gmail.com>
2024-08-15 00:41:45 +0100

    Modify undeclared function

--- test/test_mail.php
+++ test/test_mail.php
@@ -9,7 +9,7 @@ $subject = 'This is an important Message';
 $message = '<p style="font-family: Arial, sans-serif;">This Message is to verify if the emails are working well <span style="font-weight: 800"> This is a bold paragraph </span> </p>';
 
 
-if (sendEmail($userEmail, $subject, $message)) {
+if (sendMail($userEmail, $subject, $message)) {
     echo 'Test email sent successfully!';
 } else {
     echo 'Failed to send test email.';
